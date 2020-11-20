f0748e7bf87b5f7ec41ab5b425f4998dc6c4ce89
Renato Santos <renato_jfs@hotmail.com>
2020-11-20 00:18:17 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoStack' });
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
