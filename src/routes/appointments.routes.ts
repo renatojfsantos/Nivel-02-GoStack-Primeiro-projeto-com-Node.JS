933f7effed658ad8d6f172d16bb8b6531f6a1093
Renato Santos <renato_jfs@hotmail.com>
2020-11-24 21:51:28 -0300

    Appointments from routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -26,11 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
